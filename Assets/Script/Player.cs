be7efe6a8ad3aeb7db38aa8c25877a1cf34bc928
KeryanOulier <72012774+KeryanOulier@users.noreply.github.com>
2022-02-12 11:20:36 +0100

    [FIX] jumping

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,31 +5,51 @@ public class Player : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
-    private bool isJumping = false;
+    private bool isJumping;
     public bool isGrounded;
+
+    public Rigidbody2D rb;
+    public SpriteRenderer spriteRenderer;
 
     public Transform leftFoot;
     public Transform rightFoot;
 
-    public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
 
-    void FixedUpdate()
+    public static Player instance;
+
+    private void Awake()
     {
-        isGrounded = Physics2D.OverlapArea(leftFoot.position, rightFoot.position);
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de PlayerMovement dans la scène");
+            return;
+        }
 
-        float horizontalVelocity = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        instance = this;
+    }
+
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             isJumping = true;
         }
 
-        MovePlayer(horizontalVelocity);
+        Flip(rb.velocity.x);
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapArea(leftFoot.position, rightFoot.position);
+        MovePlayer(horizontalMovement);
     }
 
     void MovePlayer(float _horizontalMovement)
-    {
+    {       
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
 
@@ -37,6 +57,18 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(new Vector2(0f, jumpForce));
             isJumping = false;
+        }
+    }
+
+    void Flip(float _velocity)
+    {
+        if (_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
         }
     }
 }
